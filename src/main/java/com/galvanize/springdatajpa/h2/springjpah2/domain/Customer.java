ca2428ca50af74f51092ba865a2afe50ec9c2314
abutler6588 <abutler6588@gmail.com>
2021-07-01 10:13:36 -0500

    setup primary key in Customer class and added getter/setter methods

---
+++ src/main/java/com/galvanize/springdatajpa/h2/springjpah2/domain/Customer.java
@@ -0,0 +1,45 @@
+package com.galvanize.springdatajpa.h2.springjpah2.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    //@Entity needs a public non-argument constructor
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
