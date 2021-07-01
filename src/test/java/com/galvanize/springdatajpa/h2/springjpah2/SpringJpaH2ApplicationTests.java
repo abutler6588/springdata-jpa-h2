ec17eabf1d005141388289c92ce091fb49760df2
abutler6588 <abutler6588@gmail.com>
2021-07-01 10:04:00 -0500

    first commit

---
+++ src/test/java/com/galvanize/springdatajpa/h2/springjpah2/SpringJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.springdatajpa.h2.springjpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
